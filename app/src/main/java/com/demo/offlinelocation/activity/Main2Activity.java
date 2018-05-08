d8acffaa288388bbae28e72917a030d5af8516d1
Nishi <nishi@cloudzon.com>
2018-05-08 09:40:17 +0530

    build APK

--- app/src/main/java/com/demo/offlinelocation/activity/Main2Activity.java
+++ app/src/main/java/com/demo/offlinelocation/activity/Main2Activity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.design.BuildConfig;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +21,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.demo.offlinelocation.BuildConfig;
 import com.demo.offlinelocation.R;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
